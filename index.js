60f7898e408c63230a09a48f0d2cc8a99e4272dd
Pablo Castro <pablocas-tro1@outlook.com>
2023-04-02 20:43:30 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers,name){
+    const matchingDriver = [];
+    for (let driver of drivers){
+        if (driver === name || driver.toLowerCase() === name.toLowerCase()){
+            matchingDriver.push(driver);
+        }
+    } return matchingDriver;
+}
+
+function fuzzyMatch(drivers, name){
+    const matchingDriver = [];
+    for (let driver of drivers){
+        if(driver[0] === name[0]){
+            matchingDriver.push(driver);
+        }
+    } return matchingDriver;
+}
+
+function matchName(drivers,string){
+    return drivers.filter(driver => driver.name === string);
+}
